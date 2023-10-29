727ae0471bd083dbf48ebee31b07604b5d642879
Devashish <devashishjaybhaye@gmail.com>
2023-10-29 23:01:42 +0530

    Link-smooth-scrolling

---
+++ script.js
@@ -0,0 +1,15 @@
+ // Select all links with the class "scroll-link"
+ const scrollLinks = document.querySelectorAll('.scroll-link');
+
+ // Add smooth scrolling to all links
+ scrollLinks.forEach(link => {
+     link.addEventListener('click', e => {
+         e.preventDefault(); // Prevent default anchor behavior
+         const targetId = link.getAttribute('href'); // Get the target element's ID
+         const targetElement = document.querySelector(targetId); // Find the target element
+         window.scrollTo({
+             top: targetElement.offsetTop, // Scroll to the target element's top position
+             behavior: 'smooth' // Use smooth scrolling behavior
+         });
+     });
+ });
